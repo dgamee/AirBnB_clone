cf6639fe56e1f3ab37956df829c6436d89097214
Teeclever <omotoboraomotayo@gmail.com>
2023-12-09 15:09:46 -0800

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,76 +1,64 @@
 #!/usr/bin/python3
 
-"""FileStorage Module that serializes instances to a JSON file
-and deserializes JSON file to instances:
-"""
-
+"""A class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances"""
 import json
-from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.place import Place
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
 import os
+import models
 
 
 class FileStorage:
-    """FileStorage class for serializing and deserializing instances
-    to/from a JSON file.
 
-    Attributes:
-        __file_path (str): Path to the JSON file.
-        __objects (dict): A dictionary to store objects by class name and id.
-    Methods:
-        all(self): Returns the dictionary of stored objects.
-        new(self, obj): Adds an object to the stored objects.
-        save(self): Serializes stored objects to the JSON file.
-        reload(self): Deserializes the JSON file to restore stored objects.
-    """
+    """A Storage Engine of instances"""
+
+    dict_val = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "Place": Place,
+        "Amenity": Amenity,
+        "City": City,
+        "Review": Review,
+        "State": State,
+    }
 
     __file_path = "file.json"
     __objects = {}
-    # list to hold all classes created
-    class_list = {"BaseModel": BaseModel, "User": User,
-                  "State": State, "City": City, "Place": Place,
-                  "Amenity": Amenity, "Review": Review}
 
     def all(self):
-        """Returns the dictionary of stored objects.
-        Returns:
-            dict: A dictionary of objects.
-        """
-        return self.__objects
+        """A method that returns dictionary object"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Adds an object to the stored objects.
-        Args:
-            obj: The object to be added.
-        """
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        """A method that update the class attribute object before the save is been call"""
+
+        key = f"{obj.__class__.__name__}.{obj.id}"
+
+        FileStorage.__objects.update({key: obj})
 
     def save(self):
-        """Serializes stored objects to the JSON file.
-        """
-        serialized = {}
-        for key, value in self.__objects.items():
-            serialized[key] = value.to_dict()
-        with open(self.__file_path, mode="w", encoding="utf-8") as my_file:
-            json.dump(serialized, my_file)
+        """An object that serializes object into json file"""
+        load_instance_to_dictionaries = {
+            key: value.to_dict() for key, value in type(self).__objects.items()
+        }
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as file:
+            json.dump(load_instance_to_dictionaries, file)
 
     def reload(self):
-        """Deserializes the JSON file to restore stored objects.
-        (only if the JSON file (__file_path) exists.
-        """
-        if os.path.exists(self.__file_path):
-            try:
-                with open(self.__file_path, mode='r') as my_file:
-                    data = json.load(my_file)
-                for key, value in data.items():
-                    obj = self.class_list[value['__class__']](**value)
-                    self.__objects[key] = obj
-            except FileNotFoundError:
-                pass
+        """A method that deserialize json file"""
+        
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                hold = json.load(file)
+
+            for key, value in hold.items():
+                obj = self.dict_val[value["__class__"]](**value)
+                self.__objects[key] = obj
+        except:
+            pass
+
